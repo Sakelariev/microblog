bcc7a93cbfe80bde1da2808c1ffcc87eb3b3ae06
Sakelariev <imac@iMacs-iMac.local>
2018-05-27 11:00:32 +0300

    Flask tutorial Part 7: Error Handling

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,10 @@
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
-from app import app
+from app import app, db
 from app.forms import LoginForm, EditProfileForm
 from app.models import User
 from flask import request
 from werkzeug.urls import url_parse
-from app import db
 from app.forms import RegistrationForm
 from datetime import datetime
 
@@ -81,7 +80,7 @@ def before_request():
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
@@ -92,5 +91,3 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
-
-    
