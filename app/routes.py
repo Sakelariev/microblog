f0b689c6b2a9925d279dc1702c6aa426784c0c59
Sakelariev <imac@iMacs-iMac.local>
2018-07-22 12:40:24 +0300

    Small fix in explore route - added form.

--- app/routes.py
+++ app/routes.py
@@ -135,6 +135,7 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
+    form = PostForm()
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(
         page, app.config['POSTS_PER_PAGE'], False)
