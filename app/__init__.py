d445f11c84db01fb35819d181739bb8fd3a7bdf4
Sakelariev <ivaylo.sakelariev@gmail.com>
2018-07-26 14:28:06 +0300

    Flask Tutorials 13 and 14 (however language link doesn't show)

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,6 +20,8 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
+
 
 from app import routes, models, errors
 
@@ -49,3 +53,6 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
